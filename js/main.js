aa74c59f66d61c4ce6e6e303a11dc370927bf1cf
thanhnc1997 <thanhnc1997@gmail.com>
2022-06-16 10:27:31 +0700

    launch :3

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let template = document.querySelector('main');
+template.querySelector('header nav button').addEventListener('click', (e) => {
+	e.preventDefault();
+	e.currentTarget.classList.toggle('active');
+	template.querySelector('header nav .menu').classList.toggle('show');
+});
+
+template.querySelectorAll('aside nav .menu a').forEach((item) => {
+	item.addEventListener('click', (e) => {
+		template.querySelector('header nav .menu').classList.remove('show');
+	});
+});
